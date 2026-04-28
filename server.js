e3f43efcdf0353f5f1285f28383cb0454ee3afd7
Jewe1z <jessicapritchard2003@gmail.com>
2026-04-28 18:55:24 -0400

    Update database connection to use connectionString and SSL

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1); 
 app.use(session({
     secret: process.env.SESSION_CODE,
     resave: false,
     saveUninitialized: false,
-    cookie: { 
-        secure: true,
-        sameSite: 'none',
-        maxAge: 1000 * 60 * 60 * 24 
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24
     }
 }));
 
